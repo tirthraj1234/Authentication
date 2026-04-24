f9825cb966d64f3eb8531ba44a1474d494ae728f
sakshi kuvera <sakshikuvera@gmail.com>
2026-04-24 12:37:31 +0530

    commit

---
+++ pass.py
@@ -0,0 +1,14 @@
+import random
+import string
+print("welcome password genrator")
+def password_genrator():
+    length = int(input("Enter the password of length: "))
+    lowerupper = string.ascii_letters
+    digit = string.digits
+    symbols = string.punctuation
+    combine = lowerupper+digit+symbols
+    x = random.sample(combine, length)
+    password = "".join(x)
+    print(password)
+    
+password_genrator()
